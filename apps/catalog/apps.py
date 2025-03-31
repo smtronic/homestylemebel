4d1e877c97eb79da4bd9c86181ccd69e0c36c7b7
smtronic <aasmolnikov@gmail.com>
2025-03-31 20:50:05 +0300

    chore(migrations): rebuild catalog migrations and update constraints

--- apps/catalog/apps.py
+++ apps/catalog/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CatalogConfig(AppConfig):
-    default_auto_field = "django.db.models.UUIDField"
+    # default_auto_field = "django.db.models.UUIDField"
     name = "apps.catalog"
