8fe13e2bc7f7126898ed414f66321e1179954de6
smtronic <aasmolnikov@gmail.com>
2025-04-15 13:37:16 +0300

    feat(filters): add product filters and permissions

---
+++ apps/catalog/filters.py
@@ -0,0 +1,24 @@
+from django_filters import rest_framework as django_filters
+from apps.catalog.models import Product, Category
+
+
+class ProductFilter(django_filters.FilterSet):
+    min_price = django_filters.NumberFilter(
+        field_name="price", lookup_expr="gte", label="Цена от"
+    )
+    max_price = django_filters.NumberFilter(
+        field_name="price", lookup_expr="lte", label="Цена до"
+    )
+    in_stock = django_filters.BooleanFilter(
+        field_name="stock", lookup_expr="gt", label="В наличии"
+    )
+    category = django_filters.ModelChoiceFilter(
+        queryset=Category.objects.all(),
+        field_name="category__slug",
+        to_field_name="slug",
+        label="Категория",
+    )
+
+    class Meta:
+        model = Product
+        fields = ["category", "min_price", "max_price", "in_stock"]
